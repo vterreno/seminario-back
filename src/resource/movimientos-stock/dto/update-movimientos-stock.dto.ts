c219bccdaf607a8a81556b00d43c66d6e3629a9e
Valentino Terreno <ninot2016@gmail.com>
2025-09-28 14:48:57 -0300

    fix: nombre de dtos de movimientos de stock

--- src/resource/movimientos-stock/dto/update-movimientos-stock.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMovimientosStockDto } from './create-movimientos-stock.dto';
-
-export class UpdateMovimientosStockDto extends PartialType(CreateMovimientosStockDto) {}
